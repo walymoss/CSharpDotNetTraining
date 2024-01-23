d7179a33f837b6c87b824e6b8b7c559a262be7b7
Walter Estrada <walter.estrada.r@outlook.com>
2024-01-23 15:53:33 -0500

    Working with directories

--- Start/Files/Directories/Program.cs
+++ Start/Files/Directories/Program.cs
@@ -4,21 +4,49 @@
 const string dirname = "TestDir";
 
 // TODO: Create a Directory if it doesn't already exist
-
+if(!Directory.Exists(dirname)){
+    Directory.CreateDirectory(dirname);
+}
+else{
+    Directory.Delete(dirname);
+}
 
 // TODO: Get the path for the current directory
-
+string curpath = Directory.GetCurrentDirectory();
+// Console.WriteLine(Directory.GetCurrentDirectory());
 
 // TODO: Just like with files, you can retrieve info about a directory
-
+// DirectoryInfo directoryInfo = new DirectoryInfo(dirname);
+// Console.WriteLine($"{directoryInfo.Name}");
+// Console.WriteLine($"{directoryInfo.Parent}");
+// Console.WriteLine($"{directoryInfo.CreationTime}");
 
 // TODO: Enumerate the contents of directories
 // Console.WriteLine("Just directories:");
+// List<string> thedirs = Directory.EnumerateDirectories(curpath).ToList();
+// foreach (var str in thedirs)
+// {
+//     Console.WriteLine(str);
+// }
 
-// Console.WriteLine("---------------");
 
-// Console.WriteLine("Just files:");
 
-// Console.WriteLine("---------------");
+Console.WriteLine("---------------");
 
-// Console.WriteLine("All directory contents:");
+Console.WriteLine("Just files:");
+List<string> thefiles = Directory.EnumerateFiles(curpath).ToList();
+foreach (var str in thefiles)
+{
+    Console.WriteLine(str);
+}
+
+
+ Console.WriteLine("---------------");
+
+ Console.WriteLine("All directory contents:");
+
+thefiles = Directory.EnumerateFileSystemEntries(curpath).ToList();
+foreach (var str in thefiles)
+{
+    Console.WriteLine(str);
+}
