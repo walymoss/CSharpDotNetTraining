83664d0930563c46c575bef8a554501652b7253c
Walter Estrada <walter.estrada.r@outlook.com>
2024-01-23 14:50:52 -0500

    Creating and reading files

--- Start/Files/CreateFiles/Program.cs
+++ Start/Files/CreateFiles/Program.cs
@@ -5,6 +5,22 @@ const string filename = "TestFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
+// using(StreamWriter sw = File.CreateText(filename))
+// {
+//     sw.WriteLine("This is a text file.");
+// }
 
 
 // TODO: Determine if a file exists
+Console.WriteLine(File.Exists(filename));
+if (File.Exists(filename))
+{
+    File.Delete(filename);
+}
+else{
+     using(StreamWriter sw = File.CreateText(filename))
+    {
+        sw.WriteLine("This is a text file.");
+    }
+}
+Console.WriteLine(File.Exists(filename));
